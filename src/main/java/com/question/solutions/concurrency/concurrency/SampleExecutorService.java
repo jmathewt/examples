53c833461380352d6dd836efca309416603c6439
Jerin Mathew <jerin.mathew@tradingscreen.com>
2017-11-14 17:48:13 -0500

    rename executor service

--- src/main/java/com/question/solutions/concurrency/concurrency/SampleExecutorService.java
+++ src/main/java/com/question/solutions/concurrency/concurrency/SampleExecutorService.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class ExecutorService {
+public class SampleExecutorService {
 	
 	public static void main(String... args) throws InterruptedException, ExecutionException {
 		ExecutorService executorService = Executors.newFixedThreadPool(10);
